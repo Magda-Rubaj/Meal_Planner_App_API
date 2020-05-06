fd37252b8415e33533adb4690b33c463651ad1e2
0Weltschmerz0 <magiru@o2.pl>
2020-05-06 16:23:35 +0200

    writing tests for backend

--- Planner_app/planner/tests.py
+++ Planner_app/planner/tests.py
@@ -1,9 +1,12 @@
 import json
 from django.test import TestCase
+from rest_framework.test import force_authenticate
 from .models import CustomUser
 from rest_framework.test import APITestCase
 
-class UserTestCase(APITestCase):
+
+
+class PostUserTestCase(APITestCase):
 
     def setUp(self):
 
@@ -22,7 +25,7 @@ class UserTestCase(APITestCase):
             'desiredWeight' : 70
         }
         
-    def test__valid_user(self):
+    def test_valid_user(self):
         response = self.client.post(
             'http://127.0.0.1:8000/api/users/',
             data=json.dumps(self.valid_user),
@@ -30,7 +33,7 @@ class UserTestCase(APITestCase):
             )
         self.assertEqual(response.status_code, 201)
 
-    def test__invalid_user(self):
+    def test_invalid_user(self):
         response = self.client.post(
             'http://127.0.0.1:8000/api/users/',
             data=json.dumps(self.invalid_user),
@@ -38,6 +41,35 @@ class UserTestCase(APITestCase):
             )
         self.assertEqual(response.status_code, 400)
 
-class PermissionTestCase(TestCase):
-    pass
+class GetUsersTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create(
+            username='user', email='example@example.com', password='pass', currentWeight='123', desiredWeight='23')
+    
+    def test_get_unauthorized(self):
+        response = self.client.get('http://127.0.0.1:8000/api/users/')
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_authorized(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get('http://127.0.0.1:8000/api/users/')
+        self.assertEqual(response.status_code, 200)
+
+class PaginationTestCase(APITestCase):
+
+    def setUp(self):
+        user = CustomUser.objects.create(
+            username='user', email='example@example.com', password='pass', currentWeight='123', desiredWeight='23')
+        self.client.force_authenticate(user=user)
+
+    def test_get_valid_page(self):
+        response = self.client.get('http://127.0.0.1:8000/api/products/?page=1')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_invalid_page(self):
+        response = self.client.get('http://127.0.0.1:8000/api/products/?page=12345')
+        self.assertEqual(response.status_code, 404)
+
+
 
